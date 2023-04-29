df0776ae27db0cedab4fc8ef0b39b5d640a3c23b
abdus_shobhan <shobhanrahman21@gmail.com>
2023-04-29 15:40:07 +0600

    array_js

---
+++ array.js
@@ -0,0 +1,42 @@
+console.log(4)
+console.log("Abdus Shobhan "+ 4);
+var age=[21,34,22,11,23,23];
+for(var i=0;i<6;i++){
+    console.log("This is a  Group of Number:"+age[i]);
+
+}
+// set element of an array
+ var  element = age[2]= 21;
+ console.log(element);
+//  Find index of an element
+var find_index=age.indexOf(11);
+console.log(find_index);
+
+// Add of remove element of an array!
+
+var number =[21,34,23,54,65];
+number.push(12);
+number.pop(12);
+number.unshift(1);
+
+ console.log(number)
+//   Compare variables and Comparison operator
+console.log(2!=2);
+
+var iphone=79000;
+var budget=95000;
+if(iphone<budget){
+    console.log("I will buy Iphone");
+}
+var isGraduate=true;
+var salary=25000;
+if(isGraduate=true && salary>50000){
+
+    console.log("tor kopale beya nai");
+}
+else{
+    console.log("Ha ha");
+}
+
+
+
